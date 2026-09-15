702df1b1c2fce0643433f40d61b89f66417eaf05
isaiahrodolfo <planetquack@gmail.com>
2026-09-15 15:49:45 -0700

    added search bar

--- src/app/sermons.tsx
+++ src/app/sermons.tsx
@@ -1,5 +1,22 @@
-import { Text } from "react-native";
+import * as React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default function Sermons() {
-  return <Text>Sermons</Text>;
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Search sermons"
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", padding: 20 },
+  input: { height: 40, borderWidth: 1, borderColor: "#ccc", padding: 10 },
+});

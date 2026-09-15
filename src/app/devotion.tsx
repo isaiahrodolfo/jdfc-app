1d1c811d17a98238772096a4f2fc908b3520ed7a
isaiahrodolfo <planetquack@gmail.com>
2026-09-15 15:29:25 -0700

    add icons for pages

---
+++ src/app/devotion.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Devotion() {
+  return <Text>Devotion</Text>;
+}
---
+++ src/app/education.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Education() {
+  return <Text>Education</Text>;
+}

66a0157e1ba9de0891ec994e48fed389d7b526a6
isaiahrodolfo <planetquack@gmail.com>
2026-09-15 17:18:23 -0700

    remove (tabs) folder

--- src/app/sermons/[id].tsx
+++ src/app/sermons/[id].tsx
@@ -1,13 +1,39 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
+
+type PageMode = "description" | "notes";
+
 export default function SermonPage() {
+  const [pageMode, setPageMode] = useState<PageMode>("description");
   const { id, title } = useLocalSearchParams();
 
-  return (
+  const handleTakeNotesPress = () => {
+    setPageMode("notes");
+  };
+
+  const DescriptionPage = () => (
     <View>
       <Stack.Screen options={{ title: title.toString() || "Default Title" }} />
       <Text>Sermon Page</Text>
       <Text>Sermon ID: {id}</Text>
+      <Pressable onPress={handleTakeNotesPress}>
+        <Text>Take Notes</Text>
+      </Pressable>
     </View>
   );
+
+  const NotesPage = () => (
+    <View>
+      <Stack.Screen options={{ title: title.toString() || "Default Title" }} />
+      <Text>Notes Page</Text>
+      <Text>Sermon ID: {id}</Text>
+    </View>
+  );
+
+  if (pageMode === "description") {
+    return <DescriptionPage />;
+  } else {
+    return <NotesPage />;
+  }
 }

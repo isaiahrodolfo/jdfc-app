7cb14f7eed7ed8de358d28bb953d4eb7d05c3490
isaiahrodolfo <planetquack@gmail.com>
2026-09-16 12:58:42 -0700

    showing hello from supabase

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,28 @@
-// app/index.tsx (Home Screen)
+import { supabase } from "@/lib/supabase";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
-export default function HomeScreen() {
+export default function Home() {
+  const [titles, setTitles] = useState<string[]>([]);
+
+  useEffect(() => {
+    supabase
+      .from("todos")
+      .select()
+      .then(({ data, error }) => {
+        if (error) {
+          setTitles([error.message]);
+          return;
+        }
+        setTitles(data.map((todo) => todo.title));
+      });
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Welcome to the Home Page!</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {titles.map((title) => (
+        <Text key={title}>{title}</Text>
+      ))}
     </View>
   );
 }

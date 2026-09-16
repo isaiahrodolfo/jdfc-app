56730c910887bde554af88e4c5c9f485d37c8da0
isaiahrodolfo <planetquack@gmail.com>
2026-09-15 17:01:47 -0700

    use expo's dynamic titling

--- src/app/sermons/[id].tsx
+++ src/app/sermons/[id].tsx
@@ -1,10 +1,11 @@
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
 export default function SermonPage() {
   const { id, title } = useLocalSearchParams();
 
   return (
     <View>
+      <Stack.Screen options={{ title: title.toString() || "Default Title" }} />
       <Text>Sermon Page</Text>
       <Text>Sermon ID: {id}</Text>
     </View>
--- src/app/sermons/_layout.tsx
+++ src/app/sermons/_layout.tsx
@@ -4,12 +4,7 @@ export default function StackLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Sermons" }} />
-      <Stack.Screen
-        name="[id]"
-        options={({ route }) => ({
-          title: route.params?.title || "Default Title",
-        })}
-      />
+      <Stack.Screen name="[id]" options={{ title: "Sermon Details" }} />
     </Stack>
   );
 }
